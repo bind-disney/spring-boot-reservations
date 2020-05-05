375781d0d4a98a7466c63066c3262a1eb5dd1cc1
Артем Филатов <bind.disney@gmail.com>
2020-05-05 20:50:31 +0300

    Initial commit

---
+++ src/test/kotlin/com/binddisney/spring/boot/reservations/ReservationsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.binddisney.spring.boot.reservations
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReservationsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
